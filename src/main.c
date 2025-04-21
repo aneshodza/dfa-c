5cbc8e65ed2e0ea5adab8e9534d9a141aa8a944c
Anes Hodza <hodzaane@students.zhaw.ch>
2025-04-21 17:12:04 +0200

    Remove commented out code

--- src/main.c
+++ src/main.c
@@ -16,17 +16,7 @@ int main() {
     &q0,
     &q1
   };
-  //
-  // State *collected_states[MAX_STATES];
-  // int state_count = 0;
-  //
-  // collect_states(dfa.start, collected_states, &state_count);
-  // printf("State count: %d\n", state_count);
-  // printf("Found following states:\n");
-  // for (int i = 0; i < state_count; i++) {
-  //   printf("  - q%c\n", collected_states[i]->name);
-  // }
-  //
+
   print_graphviz(&dfa);
 
   State *computed = dfa_compute(&dfa, dfa.start, "atata");
